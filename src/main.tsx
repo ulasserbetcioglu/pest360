0e432afa4170803217537127ac7aaba7fcf84537
ulasserbetcioglu <46876065+ulasserbetcioglu@users.noreply.github.com>
2026-02-07 15:30:26 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { LanguageProvider } from './hooks/useLanguage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <LanguageProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </LanguageProvider>
   </StrictMode>
 );
